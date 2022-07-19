996af2f4e0bfa0e1907fd05d7387b584f7e83bf7
MuammerSo <muammersomali1@gmail.com>
2022-07-20 00:19:42 +0300

    Initial commit - added jump game problem and possible solutions.

---
+++ src/com/muammerso/jumpgame/JumpGame.java
@@ -0,0 +1,28 @@
+package com.muammerso.jumpgame;
+
+public class JumpGame {
+
+    public static boolean canJump(int[] inputArray){
+        int max = inputArray[0];
+
+        for(int i=0; i<inputArray.length; i++){
+            //if not enough to go to next
+            if(max <= i && inputArray[i] == 0)
+                return false;
+            //update max
+            if(i + inputArray[i] > max){
+                max = i + inputArray[i];
+            }
+            //max is enough to reach the end
+            if(max >= inputArray.length-1)
+                return true;
+        }
+        return false;
+        /* Alternative Solution, maybe better
+        int i = 0;
+        for (int reach = 0; i < inputArray.length && i <= reach; ++i)
+            reach = Math.max(reach, i + inputArray[i]);
+        return i == inputArray.length;
+         */
+    }
+}
